a55618a1410382b34b9daf242e9da9c6e5dbb3c1
Marc Bouvier <m.bouvier.dev@gmail.com>
2022-07-05 22:49:53 +0200

    Remove scaffolding noise

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+   <!--
+        <Counter/>
+      -->
+  TODO: add Counter component here
   </main>
 </template>
--- tests/unit/Counter.spec.js
+++ tests/unit/Counter.spec.js
@@ -1,47 +1,12 @@
-import {mount} from "@vue/test-utils";
-import Counter from "@/components/Counter"
 
 
 describe('Counter', () => {
 
-    let wrapper
 
-    function createComponent() {
-        wrapper = mount(Counter);
-    }
 
-    const findNumberValue = () => wrapper.find('[data-testid=counter-number-value]')
-    const findMinusButton = () => wrapper.find('[data-testid=counter-minus-button]')
-    const findPlusButton = () => wrapper.find('[data-testid=counter-plus-button]')
-
-    it('mounts without configuration', () => {
-        // smoke test (temporary : to be removed)
-        createComponent()
+    it('somke test to verify framework works', () => {
+        expect(true).toBe(true)
+        // TODO: delete me
     })
 
-
-// We can design upfront component behaviors with it.todo
-    it('renders a count value', () => {
-        createComponent()
-        expect(findNumberValue().text()).toContain('0')
-    })
-    it('renders a minus button', () => {
-        createComponent()
-        expect(findMinusButton().text()).toContain('-')
-    })
-    it('renders a plus button', () => {
-        createComponent()
-        expect(findPlusButton().text()).toContain('+')
-    })
-
-
-    it.todo('increments the count on + button click',()=>{
-        
-    })
-
-
-
-    afterEach(() => {
-        wrapper.unmount()
-    })
 })
