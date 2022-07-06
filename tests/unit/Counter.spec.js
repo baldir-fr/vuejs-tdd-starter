10b5aa4410624d380380276dff24048585dc3a15
Marc Bouvier <m.bouvier.dev@gmail.com>
2022-07-06 17:51:35 +0200

    skip next to refactor

--- tests/unit/Counter.spec.js
+++ tests/unit/Counter.spec.js
@@ -6,12 +6,13 @@ describe("Counter", () => {
     expect(true).toBe(true);
     // TODO: delete me
   });
-
+// Laisser le premier test tel quel
+  // Ligne 16 : sur le 2eme test. Le resultat du text dans une méthode doit valoir 1
   it("Should component return 0",()=>{
     expect(mount(Counter).text()).toBe("0");
   });
 
-  it("Should component return 1",()=>{
+  it.skip("Should component return 1",()=>{
     expect(mount(Counter).text()).toBe("1");
   });
 });
