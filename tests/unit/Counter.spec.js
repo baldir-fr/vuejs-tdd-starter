e9c4b3dbfd83cd4eca9a94958ae8156e7dcf7233
TEIXEIRA <thomas.teixeira@capgemini.com>
2022-07-06 17:33:43 +0200

    step 1

--- tests/unit/Counter.spec.js
+++ tests/unit/Counter.spec.js
@@ -1,12 +1,13 @@
+const { mount } = require("@vue/test-utils");
 
+describe("Counter", () => {
+  it("somke test to verify framework works", () => {
+    expect(true).toBe(true);
+    // TODO: delete me
+  });
 
-describe('Counter', () => {
+  it("Should component return 0",()=>{
+    expect(mount(Counter).text()).toBe("0");
 
-
-
-    it('somke test to verify framework works', () => {
-        expect(true).toBe(true)
-        // TODO: delete me
-    })
-
-})
+  });
+});
